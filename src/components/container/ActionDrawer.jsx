73459158164d9312a84b145f65e74bf982ab76c6
Sean <michaelbrown.sean@gmail.com>
2024-03-31 17:13:11 -0700

    drawer functional edits

---
+++ src/components/container/ActionDrawer.jsx
@@ -0,0 +1,63 @@
+import Drawer from '../base/Drawer';
+
+export default function ActionDrawer() {
+
+    const optionStyle = {
+        backgroundColor: '#73392D',
+        color: '#FFFFFF',
+    };
+
+    const selectorStyles = {
+        hoverStyle: {
+            backgroundColor: '#4E271F',
+        },
+        activeStyle: {
+            backgroundColor: '#A85442',
+        }
+    };
+
+    const options = [
+        {
+            name: 'Home',
+            href: '#',
+            ...selectorStyles,
+            style: optionStyle,
+        },
+        {
+            name: 'Post Recipe',
+            href: '#',
+            ...selectorStyles,
+            style: optionStyle,
+        },
+        {
+            name: 'Random Recipe',
+            href: '#',
+            ...selectorStyles,
+            style: optionStyle,
+        },
+    ];
+
+    const icons = [
+        "fa-solid fa-house",
+        "fa-solid fa-plus",
+        "fa-solid fa-dice"
+    ];
+
+    function handleClose() {
+        console.log("DRAWER CLOSED")
+    }
+
+    return (
+        <Drawer 
+            open={true} 
+            onClose={handleClose}
+            style={{
+                width: '15vw',
+                backgroundColor: 'var(--bg-color2)'
+            }}
+            options={options}
+            icons={icons}
+        >
+        </Drawer>
+    );
+}
