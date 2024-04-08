21bcdab155ea8be0397ab1a2ba60993fd262edce
Sean <michaelbrown.sean@gmail.com>
2024-04-08 16:48:12 -0700

    initial userprofile page design, no form

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import styles from '../styles/userprofile.module.css';
 
 import Avatar from '../images/Avatar0.jpg';
 
 export default function UserProfile() {
+  const [avatar, setAvatar] = useState('');
+
+  function handleFileChange (event) {
+    setAvatar(event.target.files[0]);
+  }
 
   return (
     <section>
@@ -16,9 +22,10 @@ export default function UserProfile() {
           </div>
           <form className={styles.avatar_form}>
             <label htmlFor="avatar"><i className="fa-solid fa-pen-to-square"></i></label>
-            <input type="file" name='avatar' id='avatar' />
+            <input type="file" name='avatar' id='avatar' onChange={handleFileChange} accept='png, jpg, jpeg'/>
           </form>
         </div>
+        <h1>TestUser</h1>
       </div>
     </section>
   );
