c04a71324a9cc926cc921b26609ae15b446854d6
Sean <michaelbrown.sean@gmail.com>
2024-04-14 23:58:50 -0700

    updated dashboard posts section

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from '../styles/dashboard.module.css';
 
 import data from '../data';
@@ -12,6 +13,21 @@ export default function Dashboard() {
         posts.length ? 
         <div className={styles.dashboard_posts}>
 
+          {posts.map(post => {
+            return (
+              <article key={post.id} className={styles.dashboard_post}>
+                <div className={styles.post_info}>
+                  <div className={styles.post_thumbnail}>
+                    <img src={post.avatar} alt="" />
+                  </div>
+                  <h4>{post.title}</h4>
+                </div>
+                <div className={styles.dashboard_buttons}>
+                  <Link></Link>
+                </div>
+              </article>
+            );
+          })}
 
         </div> : <h2>No posts available.</h2>
       }
