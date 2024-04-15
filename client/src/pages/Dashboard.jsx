000307ed47349ef63411c372acc42ef6e28b237c
Sean <michaelbrown.sean@gmail.com>
2024-04-15 00:26:19 -0700

    working version of dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ import styles from '../styles/dashboard.module.css';
 import data from '../data';
 
 export default function Dashboard() {
-  const [posts, setPosts] = useState('');
+  const [posts, setPosts] = useState(data);
 
   return (
     <section className={styles.dashboard}>
@@ -14,16 +14,20 @@ export default function Dashboard() {
         <div className={styles.dashboard_posts}>
 
           {posts.map(post => {
+            const truncatedTitle = post.title.length > 36 ? post.title.substr(0, 36) + "..." : post.title;
+
             return (
               <article key={post.id} className={styles.dashboard_post}>
                 <div className={styles.post_info}>
-                  <div className={styles.post_thumbnail}>
-                    <img src={post.avatar} alt="" />
+                  <div className={styles.post_media}>
+                    <img src={post.media} alt="" />
                   </div>
-                  <h4>{post.title}</h4>
+                  <h4>{truncatedTitle}</h4>
                 </div>
                 <div className={styles.dashboard_buttons}>
-                  <Link></Link>
+                  <Link to={`/posts/${post.id}`} className={styles.post_view}>View</Link>
+                  <Link to={`/posts/${post.id}/edit`} className={styles.post_edit}>Edit</Link>
+                  <Link to={`/posts/${post.id}/delete`} className={styles.post_delete}>Delete</Link>
                 </div>
               </article>
             );
