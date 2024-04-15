30f9108499a5d4cc0399c727cc847ee8b9838d33
Sean <michaelbrown.sean@gmail.com>
2024-04-14 23:51:32 -0700

    started dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
+import styles from '../styles/dashboard.module.css';
+
+import data from '../data';
 
 export default function Dashboard() {
+  const [posts, setPosts] = useState('');
+
   return (
-    <div>Dashboard</div>
-  )
+    <section className={styles.dashboard}>
+      { 
+        posts.length ? 
+        <div className={styles.dashboard_posts}>
+
+
+        </div> : <h2>No posts available.</h2>
+      }
+
+    </section>
+  );
 }
