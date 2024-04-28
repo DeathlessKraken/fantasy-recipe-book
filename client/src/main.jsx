d8baeba45fecb5d16b2f003102bf0435a46e9adb
Sean <michaelbrown.sean@gmail.com>
2024-04-28 12:16:02 -0700

    created login page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,13 +9,14 @@ import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/Home";
 import Recipe from "./pages/Recipe";
 import Register, { registerAction } from "./pages/Register";
+import Login, { loginAction } from "./pages/Login";
 
 /*
 Home page - browse recipes
 See specific recipe
 See specific user
 See specific users recipe posts
-Search all recipe posts
+Search all recipe posts and users
 Create recipe post
 Edit recipe post
 Delete recipe post
@@ -34,12 +35,12 @@ const router = createBrowserRouter([
             { path: "recipe/:id", element: <Recipe /> },
             //{ path: "user/:id", element: <User /> },
             //{ path: "user/:id/recipes", element: <UserPosts /> },
-            //{ path: "search/:query", element: <Search /> },
+            //{ path: "search/:query", element: <SearchContent /> },
             //{ path: "create", element: <CreateRecipe /> },
             //{ path: "recipe/:id/edit", element: <EditRecipe /> },
             //{ path: "recipe/:id/delete", element: <DeleteRecipe /> },
             { path: "register", element: <Register />, action: registerAction },
-            //{ path: "login", element: <Login /> },
+            { path: "login", element: <Login />, action: loginAction },
             //{ path: "logout", element: <Logout /> },
         ]
     }
