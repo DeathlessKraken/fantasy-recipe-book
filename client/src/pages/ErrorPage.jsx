3b094f6a4cac4bc12a86627087cd6bf0920a7a7b
Sean <michaelbrown.sean@gmail.com>
2024-04-27 17:41:50 -0700

    designed error page

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom";
 
 export default function ErrorPage () {
     return (
-        <div>
-            Error Page
-        </div>
+        <section>
+            <div className="hero min-h-screen" style={{backgroundImage: 'url(https://cdn.pixabay.com/photo/2023/06/01/16/49/castle-8033915_1280.jpg)'}}>
+              <div className="hero-overlay bg-opacity-60"></div>
+              <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                  <h1 className="mb-5 text-5xl font-bold">Oops, this page doesn&apos;t exist!</h1>
+                  <p className="mb-5">Click the button below to go back home.</p>
+                  <Link to="/">
+                    <button className="btn btn-info">Go Home</button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+        </section>
     );
 }
