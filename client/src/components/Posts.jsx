de48aa670927efc074e4d8ed094382d9ef1d1689
Sean <michaelbrown.sean@gmail.com>
2024-04-27 17:20:50 -0700

    updated card gap and position

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -8,11 +8,11 @@ export default function Posts () {
     const descriptionLength = 150;
     
     return (
-      <section className="lg:grid lg:grid-cols-3">
+      <section className="lg:grid lg:grid-cols-3 lg:gap-16 lg:p-16">
         {posts.length > 0 ? posts.map((post) => {
             return (
                 <div key={post.id} className="card bg-slate-200 shadow-xl m-4">
-                  <figure><img className="object-cover w-96 h-60" src={post.media} alt={post.title}/></figure>
+                  <figure><img className="object-cover w-full h-60" src={post.media} alt={post.title}/></figure>
                   <div className="card-body">
                     <h2 className="card-title text-default">{post.title.length > titleLength ? post.title.slice(0, titleLength) + "..." : post.title}</h2>
                     <p className="text-default">{post.body.length > descriptionLength ? post.body.slice(0, descriptionLength) + "..." : post.body}</p>
