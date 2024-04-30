0bede70e67946d76641ca039d4d53b421855c49a
Sean <michaelbrown.sean@gmail.com>
2024-04-30 15:39:44 -0700

    moved profile links back to header :}

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -11,13 +11,16 @@ export default function Sidebar () {
             <Link to="/" className=" hover:bg-slate-400 rounded-md px-4"><li className="my-2 text-xl">Home</li></Link>
             <Link to="/" className=" hover:bg-slate-400 rounded-md px-4"><li className="my-2 text-xl">Browse Recipes</li></Link>
             <Link to={isLoggedIn ? "/create/" : "/login/"} className="hover:bg-slate-400 rounded-md px-4"><li className="my-2 text-xl">Post a Recipe</li></Link>
+            <Link to="/random" className=" hover:bg-slate-400 rounded-md px-4"><li className="my-2 text-xl">Random Recipe</li></Link>
 
+            {/* 
             {isLoggedIn ? <Link to="/logout/" className="hover:bg-slate-400 rounded-md px-4"><li className="my-2 text-xl">Log Out</li></Link> :
               <>
                 <Link to="/login/" className="hover:bg-slate-400 rounded-md px-4"><li className="my-2 text-xl">Log In</li></Link>
                 <Link to="/register/" className="hover:bg-slate-400 rounded-md px-4"><li className="my-2 text-xl">Sign Up</li></Link>
               </>
             }
+            */}
 
           </ul>
         </div>
